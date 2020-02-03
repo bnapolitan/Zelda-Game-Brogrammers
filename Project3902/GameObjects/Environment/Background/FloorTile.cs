1eb6e3ff788e4c378b18fa1e6b413ec015743318
Ben Napolitan <bnapolitan@cox.net>
2020-02-03 18:12:41 -0500

    Add IEnvironment, spritesheets, objects, and refactor folders.

---
+++ Project3902/GameObjects/Environment/Background/FloorTile.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3902.GameObjects.Environment
+{
+    class FloorTile : IInteractiveEnvironmentObject
+    {
+
+        public FloorTile(ISprite sprite, Vector2 position)
+        {
+            this.Sprite = sprite;
+            this.Position = position;
+        }
+
+        public Vector2 Position { get; set; }
+        public ISprite Sprite { get; set; }
+
+        public void Activate()
+        {
+        }
+
+        public void Deactivate()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite.Texture, Position, new Rectangle(984, 11, 16, 16), Color.White);
+        }
+    }
+}
---
+++ Project3902/GameObjects/Environment/LadderTile.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3902.GameObjects.Environment
+{
+    class LadderTile : IInteractiveEnvironmentObject
+    {
+
+        public LadderTile(ISprite sprite, Vector2 position)
+        {
+            this.Sprite = sprite;
+            this.Position = position;
+        }
+
+        public Vector2 Position { get; set; }
+        public ISprite Sprite { get; set; }
+
+        public void Activate()
+        {
+        }
+
+        public void Deactivate()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite.Texture, Position, new Rectangle(1001, 45, 16, 16), Color.White);
+        }
+    }
+}
