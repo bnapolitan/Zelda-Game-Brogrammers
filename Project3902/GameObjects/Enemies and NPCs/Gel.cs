3269ad8530cae77a5bf0c7142b1648d8765d1d9a
suraj1200 <suresh.102@osu.edu>
2020-02-04 17:11:24 -0500

    Enemies Template
    Added Basic Classes/Interfaces for Enemy Characters

---
+++ Project3902/GameObjects/Enemies and NPCs/Gel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3902.GameObjects.Enemies_and_NPCs
+{
+    class Gel
+    {
+        public float Health { get; set; }
+        public Vector2 Position { get; set; }
+        public ISprite Sprite { get; set; }
+        public bool Active { get; set; }
+        public Rectangle hitbox { get; set; }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Sprite.Draw(spriteBatch);
+        }
+
+        public void OnCollide()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
