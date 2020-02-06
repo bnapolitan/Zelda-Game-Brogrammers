4fd53ab04d530c3ad6d072b9c7fb8df03b541e20
Ben Napolitan <bnapolitan@cox.net>
2020-02-06 11:54:18 -0500

    Level Modularization.

--- Project3902/LevelBuilding/Sprint2Level.cs
+++ Project3902/LevelBuilding/Sprint2Level.cs
@@ -11,20 +11,22 @@ namespace Project3902.LevelBuilding.Sprint2Level
 {
     class Sprint2Level
     {
+        private Vector2 environmentPosition;
+
         public Sprint2Level()
         {
-
+            environmentPosition = new Vector2(400, 200);
         }
 
         public List<IGameObject> CreateInteractiveEnvironmentObjects()
         {
             var list = new List<IGameObject>();
-            list.Add(EnvironmentFactory.Instance.CreateStairs(new Vector2(400, 200)));
-            list.Add(EnvironmentFactory.Instance.CreateLadderTile(new Vector2(400, 200)));
-            list.Add(EnvironmentFactory.Instance.CreateGapTile(new Vector2(400, 200)));
-            list.Add(EnvironmentFactory.Instance.CreateFire(new Vector2(400, 200)));
-            list.Add(EnvironmentFactory.Instance.CreateBrickTile(new Vector2(400, 200)));
-            list.Add(EnvironmentFactory.Instance.CreateBombedOpening(new Vector2(400, 200)));
+            list.Add(EnvironmentFactory.Instance.CreateStairs(environmentPosition));
+            list.Add(EnvironmentFactory.Instance.CreateLadderTile(environmentPosition));
+            list.Add(EnvironmentFactory.Instance.CreateGapTile(environmentPosition));
+            list.Add(EnvironmentFactory.Instance.CreateFire(environmentPosition));
+            list.Add(EnvironmentFactory.Instance.CreateBrickTile(environmentPosition));
+            list.Add(EnvironmentFactory.Instance.CreateBombedOpening(environmentPosition));
             return list;
         }
     }
