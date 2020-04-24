a3cd0cece39e4f2e116a16aba33c1177bb4f862b
Ben Napolitan <bnapolitan@cox.net>
2020-04-24 17:10:05 -0400

    Black bars on game win.

--- Project3902/Configuration/GeneralGameConfiguration.cs
+++ Project3902/Configuration/GeneralGameConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,8 @@ namespace Project3902.Configuration
         public static int EnemyFreezeTime { get; } = 300;
         public static int ScreenScrollSpeed { get; } = 8;
         public static int BombExplodeTime { get; } = 120;
+        public static float TriforceStateTimer { get; } = 9.8f;
+        public static Vector2 BlackBarsLeftPosition { get; } = new Vector2(-2300, 0);
+        public static Vector2 BlackBarsRightPosition { get; } = new Vector2(2550, 0);
     }
 }
---
+++ Project3902/GameObjects/Environment/Background/BlackBackground.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Project3902.GameObjects.Environment
+{
+    class BlackBackground : FixedGameObject, IBackgroundEnvironmentObject
+    {
+
+        public BlackBackground(Vector2 position)
+            : base(position) { }
+
+        public void MoveLeft()
+        {
+            this.Position += new Vector2(-5, 0);
+        }
+
+        public void MoveRight()
+        {
+            this.Position += new Vector2(5, 0);
+        }
+
+    }
+}
